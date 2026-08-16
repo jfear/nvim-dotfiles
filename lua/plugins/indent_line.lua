652793a92f6f7bf4f2ca0ea4f5c5ef67e907d655
Justin Fear <1240432+jfear@users.noreply.github.com>
2026-08-16 15:04:31 -0400

    refactor: migrate indent-blankline and clean kickstart remnants
    - Add lua/plugins/indent_line.lua (indent guides on blank lines)
    - Clean up plugins/init.lua: remove commented references for
      debug, autopairs, neo-tree, gitsigns, indent_line
    - Remove kickstart/plugins/indent_line.lua reference

---
+++ lua/plugins/indent_line.lua
@@ -0,0 +1,3 @@
+-- Indentation guides even on blank lines
+vim.pack.add({ "https://github.com/lukas-reineke/indent-blankline.nvim" })
+require("ibl").setup({})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,32 +1,20 @@
--- Load plugin modules in order.
-
-
--- require 'kickstart.plugins.debug'
--- require 'kickstart.plugins.indent_line'
--- require 'kickstart.plugins.lint'
--- require 'kickstart.plugins.autopairs'
--- require 'kickstart.plugins.neo-tree'
--- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
-
--- NOTE: You can add your own plugins, configuration, etc from `lua/plugins/**/*.lua`
-
 -- Auto-load all plugin config files under lua/plugins/ (excluding init.lua)
-local plugins_dir = vim.fs.joinpath(vim.fn.stdpath("config"), "lua", "plugins")
+local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'plugins')
 
 ---@param dir string
 ---@param prefix string
 local function load_plugins(dir, prefix)
-	for file_name, type in vim.fs.dir(dir, { follow = true }) do
-		local path = vim.fs.joinpath(dir, file_name)
-		if type == "directory" then
-			load_plugins(path, prefix .. file_name .. ".")
-		elseif (type == "file" or type == "link") and file_name:match("%.lua$") and file_name ~= "init.lua" then
-			local module = prefix .. file_name:gsub("%.lua$", "")
-			require(module)
-		end
-	end
+  for file_name, type in vim.fs.dir(dir, { follow = true }) do
+    local path = vim.fs.joinpath(dir, file_name)
+    if type == 'directory' then
+      load_plugins(path, prefix .. file_name .. '.')
+    elseif (type == 'file' or type == 'link') and file_name:match '%.lua$' and file_name ~= 'init.lua' then
+      local module = prefix .. file_name:gsub('%.lua$', '')
+      require(module)
+    end
+  end
 end
 
-load_plugins(plugins_dir, "plugins.")
+load_plugins(plugins_dir, 'plugins.')
 
 -- vim: ts=2 sts=2 sw=2 et

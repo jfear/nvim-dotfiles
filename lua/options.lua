752177ee42957de8eb5f963a13dbc608abeb0d68
Justin Fear <1240432+jfear@users.noreply.github.com>
2026-08-16 15:19:42 -0400

    style: reorganize options.lua with logical groupings
    - Group options by category (UI, Search, Editing, Clipboard, etc.)
    - Add :help references for every option
    - Merge duplicate formatoptions into single jcroqlnt1

--- lua/options.lua
+++ lua/options.lua
@@ -1,146 +1,60 @@
--- Enable faster startup by caching compiled Lua modules
+-- Enable faster startup by caching compiled Lua modules.
+-- See `:help vim.loader.enable()`
 vim.loader.enable()
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- ============================================================================
+-- Leader key (must be set before plugins are loaded)
+-- ============================================================================
+-- Set <space> as the leader key.
+-- See `:help mapleader` and `:help maplocalleader`
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
+-- ============================================================================
+-- Global variables
+-- ============================================================================
+-- Set to true if you have a Nerd Font installed and selected in the terminal.
+-- Used by plugins to decide whether to show icons.
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
---  See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- ============================================================================
+-- UI / Display
+-- ============================================================================
+-- Show line numbers. See `:help 'number'`
 vim.o.number = true
+
+-- Show relative line numbers. See `:help 'relativenumber'`
 vim.o.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  When connected over SSH, leave clipboard unset so OSC 52 can handle
---  clipboard sync through the terminal emulator instead.
---  See `:help 'clipboard'` and `:help clipboard-osc52`
-vim.schedule(function() vim.o.clipboard = vim.env.SSH_CONNECTION and '' or 'unnamedplus' end)
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Enable undo/redo changes even after closing and reopening a file
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Smart comment formatting:
---   q: gq formats comments      r: insert comment leader on <Enter>
---   n: numbered lists           1: don't break after 1-letter word
---   c: autoformat comments      o: insert leader after 'o'/'O'
---   j: remove comment leader when joining lines
-vim.opt.formatoptions = "qrn1coj"
-
--- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-guide-options`
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.o.confirm = true
-
--- Use spaces instead of tabs when pressing Tab or auto-indenting.
---  See `:help 'expandtab'`, `:help 'shiftwidth'`, `:help 'tabstop'`
-vim.o.expandtab = true
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-
--- ============================================================================
--- [[ Additional options inspired by LazyVim ]]
--- ============================================================================
-
--- Control how the completion menu behaves.
--- See `:help 'completeopt'`
-vim.o.completeopt = 'menu,menuone,noselect'
 
 -- Enable 24-bit RGB colors in the terminal (required for modern colorschemes).
 -- See `:help 'termguicolors'`
 vim.o.termguicolors = true
 
--- Automatically write modified buffers before certain actions (e.g. `:make`).
--- See `:help 'autowrite'`
-vim.o.autowrite = true
+-- Enable mouse mode (clicking, scrolling, resizing splits).
+-- See `:help 'mouse'`
+vim.o.mouse = "a"
 
--- Configure automatic formatting behavior when typing (e.g. comment wrapping).
--- See `:help 'formatoptions'`
-vim.o.formatoptions = 'jcroqlnt'
+-- Don't show the mode since it's already in the status line.
+-- See `:help 'showmode'`
+vim.o.showmode = false
 
--- Use ripgrep for `:grep` with a vim-friendly output format.
--- See `:help 'grepprg'` and `:help 'grepformat'`
-vim.o.grepprg = 'rg --vimgrep'
-vim.o.grepformat = '%f:%l:%c:%m'
+-- Always show the sign column (gitsigns, diagnostics, etc.) to prevent text
+-- from jumping when signs appear or disappear.
+-- See `:help 'signcolumn'`
+vim.o.signcolumn = "yes"
 
--- Command-line completion mode: complete longest common prefix first, then
--- show full matches on subsequent presses.
--- See `:help 'wildmode'`
-vim.o.wildmode = 'longest:full,full'
+-- Highlight the line where the cursor is.
+-- See `:help 'cursorline'`
+vim.o.cursorline = true
 
--- Suppress certain verbose messages (written, intro screen, completion, etc.).
--- See `:help 'shortmess'`
-vim.opt.shortmess:append { W = true, I = true, c = true, C = true }
+-- Preview substitutions live, as you type.
+-- See `:help 'inccommand'`
+vim.o.inccommand = "split"
 
--- Automatically insert indents in a smart way based on the current language.
--- See `:help 'smartindent'`
-vim.o.smartindent = true
-
--- Round indentation to a multiple of 'shiftwidth' when shifting lines.
--- See `:help 'shiftround'`
-vim.o.shiftround = true
-
--- Allow the cursor to move past the end of a line in visual block mode.
--- See `:help 'virtualedit'`
-vim.o.virtualedit = 'block'
-
--- Disable soft line wrapping (lines will extend beyond the window width).
--- See `:help 'wrap'`
-vim.o.wrap = false
+-- Display certain whitespace characters in the editor.
+-- See `:help 'list'` and `:help 'listchars'`
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Hide certain markup characters (e.g. * / _ in markdown) when not on the line.
 -- See `:help 'conceallevel'`
@@ -151,34 +65,170 @@ vim.o.conceallevel = 2
 vim.o.pumblend = 10
 vim.o.pumheight = 10
 
--- Number of undo levels to keep (much deeper than the default).
--- See `:help 'undolevels'`
-vim.o.undolevels = 10000
+-- ============================================================================
+-- Search
+-- ============================================================================
+-- Case-insensitive searching UNLESS \C or one or more capital letters are in
+-- the search term.
+-- See `:help 'ignorecase'` and `:help 'smartcase'`
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
--- Enable smooth scrolling (Neovim 0.10+).
--- See `:help 'smoothscroll'`
-vim.o.smoothscroll = true
+-- ============================================================================
+-- Editing / Indentation / Formatting
+-- ============================================================================
+-- Use spaces instead of tabs when pressing Tab or auto-indenting.
+-- See `:help 'expandtab'`, `:help 'shiftwidth'`, `:help 'tabstop'`
+-- and `:help 'softtabstop'`
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+
+-- Automatically insert indents based on the current language.
+-- See `:help 'smartindent'`
+vim.o.smartindent = true
+
+-- Round indentation to a multiple of 'shiftwidth' when shifting lines.
+-- See `:help 'shiftround'`
+vim.o.shiftround = true
+
+-- Continue visually indented lines when wrapping.
+-- See `:help 'breakindent'`
+vim.o.breakindent = true
+
+-- Disable soft line wrapping (lines extend beyond the window width).
+-- See `:help 'wrap'`
+vim.o.wrap = false
+
+-- Smart comment formatting and auto-wrap behavior.
+--   j: remove comment leader when joining lines
+--   c: autoformat comments
+--   r: insert comment leader on <Enter>
+--   o: insert comment leader after 'o'/'O'
+--   q: gq formats comments
+--   l: don't break long lines in insert mode
+--   n: recognize numbered lists
+--   t: auto-wrap text using textwidth
+--   1: don't break a line after a 1-letter word
+-- See `:help 'formatoptions'`
+vim.o.formatoptions = "jcroqlnt1"
+
+-- Allow the cursor to move past the end of a line in visual block mode.
+-- See `:help 'virtualedit'`
+vim.o.virtualedit = "block"
+
+-- If an operation would fail due to unsaved changes (e.g. `:q`), raise a dialog
+-- asking whether to save.
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+-- Automatically write modified buffers before certain actions (e.g. `:make`).
+-- See `:help 'autowrite'`
+vim.o.autowrite = true
+
+-- Default language for spell checking.
+-- See `:help 'spelllang'`
+vim.o.spelllang = "en"
+
+-- ============================================================================
+-- Clipboard
+-- ============================================================================
+-- Sync clipboard between OS and Neovim. Schedule after `UiEnter` because it
+-- can increase startup time. When connected over SSH, leave clipboard unset so
+-- OSC 52 can handle clipboard sync through the terminal emulator.
+-- See `:help 'clipboard'` and `:help clipboard-osc52`
+vim.schedule(function()
+	vim.o.clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus"
+end)
+
+-- ============================================================================
+-- Scrolling / Cursor positioning
+-- ============================================================================
+-- Minimal number of screen lines to keep above and below the cursor.
+-- See `:help 'scrolloff'`
+vim.o.scrolloff = 10
 
 -- Minimum number of screen columns to keep left/right of the cursor.
 -- See `:help 'sidescrolloff'`
 vim.o.sidescrolloff = 8
 
--- Start with all folds open so they don't collapse by default.
--- See `:help 'foldlevel'`, `:help 'foldmethod'`, and `:help 'foldtext'`
-vim.o.foldlevel = 99
-vim.o.foldmethod = 'indent'
-vim.o.foldtext = ''
+-- Enable smooth scrolling (Neovim 0.10+).
+-- See `:help 'smoothscroll'`
+vim.o.smoothscroll = true
+
+-- ============================================================================
+-- Windows / Splits
+-- ============================================================================
+-- Configure how new splits should be opened.
+-- See `:help 'splitright'` and `:help 'splitbelow'`
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Keep the screen as stable as possible when opening/closing splits.
+-- See `:help 'splitkeep'`
+vim.o.splitkeep = "screen"
 
 -- Minimum window width when splitting.
 -- See `:help 'winminwidth'`
 vim.o.winminwidth = 5
 
--- Keep the screen as stable as possible when opening/closing splits.
--- See `:help 'splitkeep'`
-vim.o.splitkeep = 'screen'
+-- ============================================================================
+-- Completion
+-- ============================================================================
+-- Control how the completion menu behaves.
+-- See `:help 'completeopt'`
+vim.o.completeopt = "menu,menuone,noselect"
 
--- Default language for spell checking.
--- See `:help 'spelllang'`
-vim.o.spelllang = 'en'
+-- Command-line completion mode: complete longest common prefix first, then
+-- show full matches on subsequent presses.
+-- See `:help 'wildmode'`
+vim.o.wildmode = "longest:full,full"
+
+-- ============================================================================
+-- Messages
+-- ============================================================================
+-- Suppress certain verbose messages (written, intro screen, completion, etc.).
+-- See `:help 'shortmess'`
+vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
+
+-- ============================================================================
+-- Timing
+-- ============================================================================
+-- Decrease update time for faster CursorHold events and swap file writing.
+-- See `:help 'updatetime'`
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time (affects which-key and multi-key chords).
+-- See `:help 'timeoutlen'`
+vim.o.timeoutlen = 300
+
+-- ============================================================================
+-- Undo / History
+-- ============================================================================
+-- Enable undo/redo changes even after closing and reopening a file.
+-- See `:help 'undofile'`
+vim.o.undofile = true
+
+-- Number of undo levels to keep (much deeper than the default).
+-- See `:help 'undolevels'`
+vim.o.undolevels = 10000
+
+-- ============================================================================
+-- Folding
+-- ============================================================================
+-- Start with all folds open so they don't collapse by default.
+-- See `:help 'foldlevel'`, `:help 'foldmethod'`, and `:help 'foldtext'`
+vim.o.foldlevel = 99
+vim.o.foldmethod = "indent"
+vim.o.foldtext = ""
+
+-- ============================================================================
+-- External tools
+-- ============================================================================
+-- Use ripgrep for `:grep` with a vim-friendly output format.
+-- See `:help 'grepprg'` and `:help 'grepformat'`
+vim.o.grepprg = "rg --vimgrep"
+vim.o.grepformat = "%f:%l:%c:%m"
 
 -- vim: ts=2 sts=2 sw=2 et

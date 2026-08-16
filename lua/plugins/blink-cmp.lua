117afb1373389e75db7e619661b8ddd3eeb76bcd
Justin Fear <1240432+jfear@users.noreply.github.com>
2026-08-16 15:56:13 -0400

    feat(blink-cmp): enable super-tab, preselect, and path trailing-slash for smoother completion

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,24 +1,37 @@
-vim.pack.add({
-	{ src = "https://github.com/L3MON4D3/LuaSnip", version = vim.version.range("2.*") },
-	"https://github.com/rafamadriz/friendly-snippets",
-	{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.*") },
-})
+vim.pack.add {
+  { src = 'https://github.com/L3MON4D3/LuaSnip', version = vim.version.range '2.*' },
+  'https://github.com/rafamadriz/friendly-snippets',
+  { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range '1.*' },
+}
 
-require("luasnip").setup({})
-require("luasnip.loaders.from_vscode").lazy_load()
+require('luasnip').setup {}
+require('luasnip.loaders.from_vscode').lazy_load()
 
-require("blink.cmp").setup({
-	keymap = { preset = "default" },
-	appearance = {
-		nerd_font_variant = "mono",
-	},
-	completion = {
-		documentation = { auto_show = false, auto_show_delay_ms = 500 },
-	},
-	sources = {
-		default = { "lsp", "path", "snippets" },
-	},
-	snippets = { preset = "luasnip" },
-	fuzzy = { implementation = "prefer_rust_with_warning" },
-	signature = { enabled = true },
-})
+require('blink.cmp').setup {
+  keymap = { preset = 'super-tab' },
+  appearance = {
+    nerd_font_variant = 'mono',
+  },
+  completion = {
+    list = { selection = 'preselect' },
+    documentation = { auto_show = false, auto_show_delay_ms = 500 },
+    trigger = {
+      show_on_accept_on_trigger_character = true,
+    },
+  },
+  sources = {
+    default = { 'lsp', 'path', 'snippets' },
+    providers = {
+      path = {
+        opts = {
+          trailing_slash = true,
+          label_trailing_slash = true,
+          show_hidden_files_by_default = false,
+        },
+      },
+    },
+  },
+  snippets = { preset = 'luasnip' },
+  fuzzy = { implementation = 'prefer_rust_with_warning' },
+  signature = { enabled = true },
+}
